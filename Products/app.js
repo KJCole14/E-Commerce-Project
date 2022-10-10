0a522eadf07e233306e75bb34fdd5bb70c5685b2
KJCole14 <kabahjcole@gmail.com>
2022-10-09 23:05:24 -0400

    set up products page and adding media queries

--- Products/app.js
+++ Products/app.js
@@ -1,17 +1,39 @@
 console.log("Hello World!");
 
-fetch("shop.json")
-    .then(response => response.json())
-    .then(json => {
+// fetch("shop.json")
+//     .then(response => response.json())
+//     .then(json => {
 
-        // let list = "";
-        let list = "<table>";
-        json.popItems.forEach(item => {
-            // list += `<li>${item.name} ${item.price} ${item.desc}</li>`
-            list += `<tr><td>${item.name}</td><td>${item.price}</td><td>${item.desc}</td></tr>`
-        });
-        document.querySelector(`ol`).innerHTML = list;
-        list += "</table>";
+//         // let list = "";
+//         let list = "<table>";
+//         json.popItems.forEach(item => {
+//             // list += `<li>${item.name} ${item.price} ${item.desc}</li>`
+//             list += `<tr><td>${item.image}</td><td>${item.name}</td><td>${item.price}</td><td>${item.desc}</td></tr>`
+//         });
+//         document.querySelector(`ol`).innerHTML = list;
+//         list += "</table>";
+//     })
+
+fetch("shop.json")
+
+    .then(function(response) {
+        return response.json();
     })
 
+    .then(function(products) {
+        let placeholder = document.querySelector("#data-output");
+        let out = "";
+        for(let product of products) {
+            out += `
+            <tr>
+                <td> <img src='${product.image}'></td>
+                <td>${product.name}</td>
+                <td>${product.price}</td>
+                <td>${product.desc}</td>
+            </tr>
+            
+            `;
+        }
 
+        placeholder.innerHTML = out;
+    })
